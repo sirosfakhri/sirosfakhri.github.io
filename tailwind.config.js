f281acfec4126afa841551cd1bf034eb843c86af
= <=>
2022-10-29 22:40:45 +0330

    fix dark & white theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.{html,js}"],
+  darkMode : 'class',
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    extend: {
+      colors :{
+        'body-dark' : '#212428',
+        'box-dark' : '#202327',
+      },
+    },
   },
   plugins: [],
 }
